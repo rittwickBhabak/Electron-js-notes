f8e4b4da2f136489cfb7a1a772cf0d19533a84b0
Rittwick <rittwick2019@gmail.com>
2021-05-09 21:46:39 +0530

    prot 3000 removed

--- app.js
+++ app.js
@@ -5,28 +5,25 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
-app.listen(3001);
+app.listen();
 
 
 app.get('/:id', (req, res) => {
     const id = req.params.id;
-    if(id==1)
-    {
+    if (id == 1) {
         // console.log('next'+id);
-        res.render(`chapter_${id}`, {next:+id + 1, prev: null}); 
+        res.render(`chapter_${id}`, { next: +id + 1, prev: null });
     }
-    else if(id==11)
-    {
+    else if (id == 11) {
         // console.log('prev'+id);
-        res.render(`chapter_${id}`, {prev: +id - 1, next: null});
+        res.render(`chapter_${id}`, { prev: +id - 1, next: null });
     }
-    else
-    {
+    else {
         // console.log('prev'+id+"|"+'next'+id);
-        res.render(`chapter_${id}`, {next: +id + 1, prev: +id - 1})
+        res.render(`chapter_${id}`, { next: +id + 1, prev: +id - 1 })
     }
 })
 
 app.get('/', (req, res) => {
-    res.render('index'); 
+    res.render('index');
 })
